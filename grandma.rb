d2e3e576d18a953d0147aacbae208c11dc660735
MJ Johnson <mjohnson4685@gmail.com>
2020-09-10 02:40:14 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,11 +13,11 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-if phrase == "HI!".upcase
+if phrase != "HI!".upcase
     puts "NO, NOT SINCE 1938!"
 elsif phrase == "I LOVE YOU GRANDMA!".upcase
     puts "I LOVE YOU TOO PUMPKIN!"
-else phrase != "Hi Nana, how are you?".upcase
+else phrase == "Hi Nana, how are you?".upcase
     puts " HUH?! SPEAK UP, SONNY!"
     end
 end
