50bfd014f342e71f4923a7e84deacd14e282ff71
MJ Johnson <mjohnson4685@gmail.com>
2020-09-10 02:09:16 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,7 +17,7 @@ if phrase=="#{phrase}".upcase
     puts "NO, NOT SINCE 1938!"
 elsif phrase=="I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
-else phrase ! "#{phrase}".upcase
+else phrase != "#{phrase}".upcase
     puts " HUH?! SPEAK UP, SONNY!"
     end
 end
